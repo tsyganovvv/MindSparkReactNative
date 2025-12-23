339f466cb4e1f60c269415c4705bcb49a4fa0453
Vladimir Tsyganov <tsyganov.vv0228@gmail.com>
2025-12-24 00:54:19 +0700

    update settings for sessions data

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,9 +1,64 @@
-import { Text, View, Pressable } from 'react-native';
+import { Text, View, Pressable, ActivityIndicator } from 'react-native';
 import styles from '../styles/SettingsStyle';
 import mainstyles from '../styles/MainStyles';
 import { Ionicons } from '@expo/vector-icons';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useState, useEffect } from 'react'
+
 
 export default function SettingsScreen() {
+  const [userData, setUserData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    handleData();
+  }, []);
+
+  const handleData = async () =>{
+    try {
+      const authToken = await AsyncStorage.getItem('auth_token');
+       if (!authToken) {
+        setError("No auth token")
+        return;
+       }
+      const response = await fetch('http://192.168.0.161:8000/v1/sessions/', {
+          method: 'GET',
+          headers: {
+              'Content-Type': 'application/json',
+              'Authorization': authToken,
+          },
+      });
+      if (response.ok){
+        const data = await response.json();
+        setUserData(data);
+      } else {
+        setError('Error while load data');
+      }
+    }catch(error){
+      setError('Error connection' + error.message);
+    }finally{
+      setLoading(false);
+    }
+  }
+  if (loading) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
+  if (error) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text style={{ color: 'red' }}>{error}</Text>
+        <Pressable onPress={handleData} style={{ marginTop: 10, padding: 10, backgroundColor: '#007AFF', borderRadius: 5 }}>
+          <Text style={{ color: 'white' }}>Repeat</Text>
+        </Pressable>
+      </View>
+    );
+  }
+
+
   return (
     <>
       <View style={ mainstyles.header }>
@@ -11,11 +66,11 @@ export default function SettingsScreen() {
       </View>
       <View style={styles.profileContainer}>
         <View style={styles.avatarContainer}>
-          <Text style={styles.avatarText}>V</Text>
+          <Text style={styles.avatarText}>{ userData.fullname[0] }</Text>
         </View>
         <View style={styles.profileInfo}>
-          <Text style={styles.profileName}>Vladimir Tsyganov</Text>
-          <Text style={styles.profileEmail}>tsyganov.vv0228@gmail.com</Text>
+          <Text style={styles.profileName}>{ userData.fullname }</Text>
+          <Text style={styles.profileEmail}>{ userData.email}</Text>
         </View>
       </View>
       <View>
